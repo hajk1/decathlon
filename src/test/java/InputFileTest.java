13cb4957abc6ccf7253e656f35e715dc3f5ef1aa
Kayvan Hajhassantehrani <k1.tehrani@gmail.com>
2018-08-28 14:50:55 +0430

    specifying java 8 for maven compiler plugin

--- src/test/java/InputFileTest.java
+++ src/test/java/InputFileTest.java
@@ -4,13 +4,12 @@ import ir.hajk1.facade.OutputTransformer;
 import ir.hajk1.facade.XmlTransformer;
 import ir.hajk1.model.Athlete;
 import ir.hajk1.model.AthleteListResult;
-import org.junit.jupiter.api.Test;
-
-import javax.xml.bind.JAXBException;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
+import javax.xml.bind.JAXBException;
+import org.junit.jupiter.api.Test;
 
 /**
  * Created by k1 on 8/26/18.
@@ -20,7 +19,8 @@ public class InputFileTest {
     @Test
     public void testInputFile() throws IOException {
         InputTransformer inputParser = new CSVTransformer();
-        assert inputParser.parse(new BufferedReader(new FileReader("src/test/resources/results.csv"))).length == 6;
+      assert inputParser
+          .parse(new BufferedReader(new FileReader("src/test/resources/results.csv"))).length == 7;
     }
 
     @Test
