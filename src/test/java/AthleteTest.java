c97224887cad0b05105526ef3f986e9e4f73de04
Kayvan Hajhassantehrani <k1.tehrani@gmail.com>
2018-08-28 14:54:34 +0430

    Test file separation

---
+++ src/test/java/AthleteTest.java
@@ -0,0 +1,31 @@
+import ir.hajk1.facade.OutputTransformer;
+import ir.hajk1.facade.XmlTransformer;
+import ir.hajk1.model.Athlete;
+import ir.hajk1.model.AthleteListResult;
+import java.io.IOException;
+import java.util.Arrays;
+import javax.xml.bind.JAXBException;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by k1 on 8/26/18.
+ * email:<k1.tehrani@gmail.com>
+ */
+public class AthleteTest {
+
+  @Test
+  public void testAthleteBuilder() throws IOException {
+    Athlete athlete = new Athlete(
+        "John Smith;12.61;5.00;9.22;1.50;60.39;16.43;21.60;2.60;35.81;5.25.72");
+    assert true;
+  }
+
+  @Test
+  public void testAthleteXmlBuilder() throws IOException, JAXBException {
+    Athlete athlete = new Athlete(
+        "John Smith;12.61;5.00;9.22;1.50;60.39;16.43;21.60;2.60;35.81;5.25.72");
+    OutputTransformer outputTransformer = new XmlTransformer();
+    outputTransformer
+        .marshal(new AthleteListResult(Arrays.asList(new Athlete[]{athlete})), System.out);
+  }
+}
--- src/test/java/InputFileTest.java
+++ src/test/java/InputFileTest.java
@@ -1,14 +1,8 @@
 import ir.hajk1.facade.CSVTransformer;
 import ir.hajk1.facade.InputTransformer;
-import ir.hajk1.facade.OutputTransformer;
-import ir.hajk1.facade.XmlTransformer;
-import ir.hajk1.model.Athlete;
-import ir.hajk1.model.AthleteListResult;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
-import javax.xml.bind.JAXBException;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -21,18 +15,5 @@ public class InputFileTest {
         InputTransformer inputParser = new CSVTransformer();
       assert inputParser
           .parse(new BufferedReader(new FileReader("src/test/resources/results.csv"))).length == 7;
-    }
-
-    @Test
-    public void testAthleteBuilder() throws IOException {
-        Athlete athlete = new Athlete("John Smith;12.61;5.00;9.22;1.50;60.39;16.43;21.60;2.60;35.81;5.25.72");
-        assert true;
-    }
-
-    @Test
-    public void testAthleteXmlBuilder() throws IOException, JAXBException {
-        Athlete athlete = new Athlete("John Smith;12.61;5.00;9.22;1.50;60.39;16.43;21.60;2.60;35.81;5.25.72");
-        OutputTransformer outputTransformer = new XmlTransformer();
-        outputTransformer.marshal(new AthleteListResult(Arrays.asList(new Athlete[]{athlete})), System.out);
     }
 }
